54c2241d40ecb2048b11dcc1e317cd5b3ff8cd4a
Mai Vang <vmaineng@gmail.com>
2023-10-11 18:25:49 -0700

    added in jest unit test

--- testing/src/components/greet.test.js
+++ testing/src/components/greet.test.js
@@ -1,10 +1,20 @@
-import { render, screen } from '@testing-library/react'
-import { Greet } from './greet';
-import { text } from 'express';
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getAllByText('Hello')
-    expect(textElement).toBeInTheDocument();
 
-})
+test('Greet renders Hello correctly', () => {
+    const str = 'Hello'
+    expect(str).toContain('Hello');
+});
+
+
+
+//RTL
+
+// import { render, screen } from '@testing-library/react'
+// import { Greet } from './greet';
+
+// test('Greet renders correctly', () => {
+//     render(<Greet />)
+//     const textElement = screen.getByText('Hello')
+//     expect(textElement).toBeInTheDocument(); 
+
+// })
